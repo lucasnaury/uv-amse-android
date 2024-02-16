0e51fd34aed4b559dda60d3646d0a4da2e8a37b5
Lucas NAURY <lucas.naury@sfr.fr>
2024-02-16 09:27:33 +0100

    Change page names

--- app/src/main/java/com/example/tpleboncoin/ui/ajout_annonce/AjoutAnnonceFragment.java
+++ app/src/main/java/com/example/tpleboncoin/ui/ajout_annonce/AjoutAnnonceFragment.java
@@ -1,4 +1,4 @@
-package com.example.tpleboncoin.ui.dashboard;
+package com.example.tpleboncoin.ui.ajout_annonce;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.tpleboncoin.databinding.FragmentDashboardBinding;
+import com.example.tpleboncoin.databinding.FragmentAjoutAnnonceBinding;
 
-public class DashboardFragment extends Fragment {
+public class AjoutAnnonceFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentAjoutAnnonceBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        AjoutAnnonceViewModel ajout_annonceViewModel =
+                new ViewModelProvider(this).get(AjoutAnnonceViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentAjoutAnnonceBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textAjoutAnnonce;
+        ajout_annonceViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/tpleboncoin/ui/ajout_annonce/AjoutAnnonceViewModel.java
+++ app/src/main/java/com/example/tpleboncoin/ui/ajout_annonce/AjoutAnnonceViewModel.java
@@ -1,16 +1,16 @@
-package com.example.tpleboncoin.ui.dashboard;
+package com.example.tpleboncoin.ui.ajout_annonce;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class AjoutAnnonceViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public AjoutAnnonceViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("Ajouter une annonce");
     }
 
     public LiveData<String> getText() {
