bee9c773b11b0cc8190fa3d66147ca1a4c89ae00
Lucas NAURY <lucas.naury@sfr.fr>
2024-02-16 08:52:33 +0100

    Ajout d'un ID automatique aux annonces

--- app/src/main/java/com/example/tpleboncoin/models/Annonce.java
+++ app/src/main/java/com/example/tpleboncoin/models/Annonce.java
@@ -1,18 +1,24 @@
 package com.example.tpleboncoin.models;
 
 public class Annonce {
+    static int nbAnnonces = 0;
+    private int id;
     private String titre;
     private String  adresse;
     private int image;
 
-    //Constructeur
+    // Constructeur
     public Annonce(String titre, String adresse, int image) {
         this.titre = titre;
         this.adresse = adresse;
         this.image = image;
+
+        // Génération d'un id automatique
+        this.id = Annonce.nbAnnonces;
+        Annonce.nbAnnonces++;
     }
 
-    // Getter and Setter
+    // Getter et Setter
     public String getTitre() {
         return titre;
     }
