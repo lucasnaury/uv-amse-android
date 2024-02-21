0d4812512d4890d533b7919687ea69236bd1bd63
donia <donia.sakji@gmail.com>
2024-02-21 11:56:06 +0100

    detailScreen ver4

--- app/src/main/java/com/example/tpleboncoin/ui/DetailScreen.java
+++ app/src/main/java/com/example/tpleboncoin/ui/DetailScreen.java
@@ -1,9 +1,14 @@
 package com.example.tpleboncoin.ui;
 
+import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.tpleboncoin.databinding.ActivityDetailScreenBinding;
 
 import android.content.Intent;
+import android.graphics.text.LineBreaker;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,10 +30,13 @@ public class DetailScreen extends AppCompatActivity {
                 Annonce annonce = intent.getParcelableExtra("annonce");
 
                 binding.adresseTextView.setText(annonce.getAdresse());
-                binding.prixTextView.setText( Double.toString(annonce.getPrix()) + " €");
-                binding.imageView.setImageResource(R.drawable.ic_account_circle_black_24dp);
+                binding.prixTextView.setText( String.format("%,.2f €", annonce.getPrix())) ;
                 binding.descriptionTextView4.setText(annonce.getDescription());
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    binding.descriptionTextView4.setJustificationMode(LineBreaker.JUSTIFICATION_MODE_INTER_WORD);
+                }
                 binding.titreTextView.setText(annonce.getTitre());
+                binding.imageView.setImageURI(Uri.parse("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuN0ilDzXue08UjwoRi4jiqj0Y9FWs7jDhYY949clvcVYpoCUZzH1OkTKv5-FqBHDwKG0&usqp=CAU"));
             }
             binding.smsButton.setOnClickListener(new View.OnClickListener() {
                 @Override
