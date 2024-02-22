9a3a72be9f2f282b5c003a6a4eccd142a2bef8ed
Lucas NAURY <lucas.naury@sfr.fr>
2024-02-22 20:32:50 +0100

    Modification des boutons de contact

--- app/src/main/java/com/example/tpleboncoin/ui/DetailScreen.java
+++ app/src/main/java/com/example/tpleboncoin/ui/DetailScreen.java
@@ -17,8 +17,11 @@ import android.widget.ImageButton;
 import com.example.tpleboncoin.R;
 import com.example.tpleboncoin.models.Annonce;
 
+import java.util.Objects;
+
 public class DetailScreen extends AppCompatActivity {
         ActivityDetailScreenBinding binding;
+        Annonce annonce;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -27,7 +30,7 @@ public class DetailScreen extends AppCompatActivity {
             setContentView(binding.getRoot());
             Intent intent = this.getIntent();
             if (intent != null){
-                Annonce annonce = intent.getParcelableExtra("annonce");
+                annonce = intent.getParcelableExtra("annonce");
 
                 binding.adresseTextView.setText(annonce.getAdresse());
                 binding.prixTextView.setText( String.format("%,.2f €", annonce.getPrix())) ;
@@ -42,12 +45,37 @@ public class DetailScreen extends AppCompatActivity {
                 }
 
             }
+
+            //Only show buttons if data provided in annonce
+            if(Objects.equals(annonce.getNumeroTelephone(), "")){
+                binding.smsButton.setVisibility(View.INVISIBLE);
+            }
+            if(Objects.equals(annonce.getEmail(), "")){
+                binding.emailButton.setVisibility(View.INVISIBLE);
+            }
+
+            //Buttons for SMS or email
             binding.smsButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View arg0) {
-                    Intent smsIntent = new Intent(Intent.ACTION_MAIN);
-                    smsIntent.addCategory(Intent.CATEGORY_APP_MESSAGING);
+                    Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+                    smsIntent.setData(Uri.parse("sms:"));
+                    smsIntent.setType("vnd.android-dir/mms-sms");
+                    smsIntent.putExtra(Intent.EXTRA_TEXT, "");
+                    smsIntent.putExtra("address",  annonce.getNumeroTelephone());
                     startActivity(smsIntent);
+                }
+            });
+
+
+            binding.emailButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                            "mailto", annonce.getEmail(), null));
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Réponse à l'annonce - " + annonce.getTitre());
+//                    emailIntent.putExtra(Intent.EXTRA_TEXT, "Body");
+                    startActivity(Intent.createChooser(emailIntent, "Envoi d'email"));
                 }
             });
 
